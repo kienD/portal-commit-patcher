d96088c25fdcb6ef393bcbb5fbf0dce588d5c481
Kien Do <kien.do@liferay.com>
2021-07-14 14:29:25 -0700

    Add functions to add comments to PRs Add check for diff in changes between PRs after patching

--- index.sh
+++ index.sh
@@ -4,11 +4,38 @@ set -e
 
 source config
 
+# Need to install jq tool
+
 # TODO Update this to always read files from this DIR for accessing patch.awk
 
 # This script takes the pull request id at arg1, fetches the patch for the PR and then updates the paths in the patch with the portal_path_addition provided in the config file.
 
-PULL_ID="${1}"
+ORIGIN_PULL_ID="${1}"
+
+# Add ci:foward & link comment to origin PR
+add_origin_pr_comment() {
+	local pr_body="${1}"
+
+	local response=$(curl -X POST -H "Authorization: token ${TOKEN}" -d  "{\"body\": \"${pr_body}\"}" "https://api.github.com/repos/${GITHUB_ORIGIN_USER}/${GITHUB_ORIGIN_REPO}/issues/${ORIGIN_PULL_ID}/comments")
+
+	echo $(echo "${response}" | jq '.body')
+}
+
+# Update PR origin state
+update_origin_pr_state() {
+	local state="${1}"
+
+	curl -X PATCH -H "Authorization: token ${TOKEN}" -d  "{\"state\": \"${state}\"}" "https://api.github.com/repos/${GITHUB_ORIGIN_USER}/${GITHUB_ORIGIN_REPO}/issues/${ORIGIN_PULL_ID}"
+}
+
+# Close original PR with comment linking to new PR
+add_destination_pr_comment() {
+	local pr_number="${1}"
+
+	local pr_body="ci:forward"
+
+	curl -X POST -H "Authorization: token ${TOKEN}" -d  "{\"body\": \"${pr_body}\"}" "https://api.github.com/repos/${GITHUB_DESTINATION_USER}/${GITHUB_DESTINATION_REPO}/issues/${pr_number}/comments"
+}
 
 # Apply patch to repo at patch_destination_path
 apply_patch() {
@@ -31,6 +58,8 @@ apply_patch() {
 delete_local_branch() {
 	local file_name=$(basename "${patch_file_path}" .patch)
 
+	git checkout "7.1.x"
+
 	git branch -D "${file_name}"
 }
 
@@ -38,15 +67,46 @@ delete_local_branch() {
 send_pull_request() {
 	local file_name=$(basename "${patch_file_path}" .patch)
 
-	git push origin "${file_name}"
-
 	# Update to use liferay-ac-ci user instead of my account.  Also should use github api vs hub
-	curl -X POST -H "Authorization: token ${TOKEN}" -d  "{\"title\": \"${file_name}\", \"body\": \"ci:forward\", \"head\": \"${file_name}\", \"base\": \"7.1.x\"}" "https://api.github.com/repos/${GITHUB_CI_USER}/${GITHUB_DESTINATION_REPO}/pulls"
+	# local destination_pr_response=$(curl -X POST -H "Authorization: token ${TOKEN}" -d  "{\"title\": \"${file_name}\", \"body\": \"ci:forward\", \"head\": \"${file_name}\", \"base\": \"7.1.x\"}" "https://api.github.com/repos/${GITHUB_DESTINATION_USER}/${GITHUB_DESTINATION_REPO}/pulls")
+
+	# For testing purposes
+	local destination_pr_response=$(curl -X GET -H "Authorization: token ${TOKEN}" "https://api.github.com/repos/${GITHUB_DESTINATION_USER}/${GITHUB_DESTINATION_REPO}/pulls/23")
+
+	local destination_changes=$(echo "${destination_pr_response}" | jq --raw-output '{additions: .additions, changed_files: .changed_files, deletions: .deletions} | to_entries | map("[\(.key)]=\(.value)") | reduce .[] as $item ("destination_changes_dict=("; . + ($item) + " ") + ")"')
+
+
+	# Creates destination_changes_dict
+	local -A "${destination_changes}"
+
+	local origin_pr_response=$(curl -X GET -H "Authorization: token ${TOKEN}" "https://api.github.com/repos/${GITHUB_ORIGIN_USER}/${GITHUB_ORIGIN_REPO}/pulls/${ORIGIN_PULL_ID}")
+
+	local origin_changes=$(echo "${origin_pr_response}" | jq --raw-output '{additions: .additions, changed_files: .changed_files, deletions: .deletions} | to_entries | map("[\(.key)]=\(.value)") | reduce .[] as $item ("origin_changes_dict=("; . + ($item) + " ") + ")"')
+
+	# Creates origin_changes_dict
+	local -A "${origin_changes}"
+
+	local change_errors
+
+
+	for key in "${!destination_changes_dict[@]}"; do
+		if [[ "${destination_changes_dict[${key}]}" != "${origin_changes_dict[${key}]}" ]]; then
+			change_errors="${change_errors}**${key}** count does not match\n"
+		fi
+	done
+
+
+	if [[ -n "${change_errors}" ]]; then
+		# Send change errors as comment to original PR
+		add_origin_pr_comment "### Patching Failed:\n${change_errors}"
+
+		exit 1
+	fi
+
+	echo "${destination_pr_response}"
 }
 
 update_origin_base_branch() {
-	cd "${PATCH_DESTINATION_PATH}"
-
 	current_branch=$(git branch --show-current)
 
 	if [[ $current_branch != "${BASE_BRANCH}" ]]; then
@@ -73,7 +133,7 @@ create_updated_patch() {
 
 		current_line_string=$(sed "${lineNumber}q;d" "${file_path}")
 
-		if [[ "${current_line_string}" == *"${PORTAL_PATH_ADDITION}"* ]]; then
+		if [[ "${current_line_string}" == *"${DESTINATION_PATH_ADDITION}"* ]]; then
 			echo "Skipping line ${lineNumber}: Already converted"
 		else
 			sed -i "${lineNumber}s~^.*$~${lineContent}~" "${file_path}"
@@ -86,14 +146,14 @@ create_updated_patch() {
 }
 
 fetch_patch() {
-	curl -H "Authorization: token ${TOKEN}" -H "Accept: application/vnd.github.v3.patch" "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/pulls/${PULL_ID}" > "${PATCH_FILE_SAVE_LOCATION}/pull-${PULL_ID}.patch"
+	curl -H "Authorization: token ${TOKEN}" -H "Accept: application/vnd.github.v3.patch" "https://api.github.com/repos/${GITHUB_ORIGIN_USER}/${GITHUB_ORIGIN_REPO}/pulls/${ORIGIN_PULL_ID}" > "${PATCH_FILE_SAVE_LOCATION}/pull-${ORIGIN_PULL_ID}.patch"
 
-	echo "${PATCH_FILE_SAVE_LOCATION}/pull-${PULL_ID}.patch"
+	echo "${PATCH_FILE_SAVE_LOCATION}/pull-${ORIGIN_PULL_ID}.patch"
 }
 
 main() {
-	if [[ -z "${PULL_ID}" ]]; then
-		echo "A PULL_ID is required: ${0} PULL_ID"
+	if [[ -z "${ORIGIN_PULL_ID}" ]]; then
+		echo "A ORIGIN_PULL_ID is required: ${0} ORIGIN_PULL_ID"
 
 		exit 1
 	fi
@@ -101,15 +161,35 @@ main() {
 	patch_file_path=$(fetch_patch)
 
 	# Create a file of the updated lines prepended with the line numbers
-	updated_lines=$(awk -f ./patch.awk -v portal_path_addition=${PORTAL_PATH_ADDITION} ${patch_file_path})
+	local updated_lines=$(awk -f ./patch.awk -v portal_path_addition="${DESTINATION_PATH_ADDITION}" "${patch_file_path}")
 
 	create_updated_patch	"${patch_file_path}" "${updated_lines}"
+
+	cd "${PATCH_DESTINATION_PATH}"
 
 	update_origin_base_branch
 
 	apply_patch "${patch_file_path}"
 
-	send_pull_request
+	local file_name=$(basename "${patch_file_path}" .patch)
+
+	git push origin "${file_name}"
+
+	local pr_response=$(send_pull_request)
+
+	local pr_number=$(echo "${pr_response}" | jq '.number')
+	local pr_url=$(echo "${pr_response}" | jq '.html_url')
+
+	local origin_comment_body=$(add_origin_pr_comment "PR forwarded to [here](${pr_url})")
+
+	if [[ -n  "${origin_comment_body}" ]]; then
+		update_origin_pr_state "closed"
+	else
+		echo "Failed to add origin pr comment"
+	fi
+
+	echo "${pr_number}"
+	add_destination_pr_comment "${pr_number}"
 
 	delete_local_branch
 
